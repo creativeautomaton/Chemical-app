76e4ae255e616d2e6d52130d23dac0e4195cb710
KraftZ <KraftZ@stlouis-mo.gov>
2017-08-29 15:02:02 -0500

    latest

--- src/dynamicForm/services/question.service.ts
+++ src/dynamicForm/services/question.service.ts
@@ -161,60 +161,500 @@ export class AssessmentQuestionService {
         order: 1
       }),
 
+   // User Information
       new TextboxQuestion({
-        key: 'ProcessTitle',
-        label: 'Process Title',
+        key: 'company',
+        label: 'Company',
         value: ' ',
         required: false,
         order: 2
       }),
-
       new TextboxQuestion({
-        key: 'Location',
+        key: 'department',
+        label: 'Department',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'section',
+        label: 'Section Name',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'building',
+        label: 'Building Name',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'date',
+        label: 'Date',
+        value: ' ',
+        required: true,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'location',
         label: 'Location',
         value: ' ',
         required: false,
         order: 2
       }),
-
+    // Chemical Table
       new TextboxQuestion({
-        key: 'RiskAssesor',
-        label: 'Risk Assesor',
+        key: 'chemicalName',
+        label: 'Chemical Name (SDS):',
+        value: ' ',
+        required: true,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'chemicalManufacturer',
+        label: 'Chemical Manufacturer:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'category',
+        label: 'Product Category:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'boiling',
+        label: 'Boiling Point (°F):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'freezing',
+        label: 'Freezing Point (°F):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'solubility',
+        label: 'Solubility (%):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'evaporationRate',
+        label: 'Evaporation Rate:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'upperFlammability',
+        label: 'Upper Flammability (%):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'solubility',
+        label: 'Solubility (%):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'vaporDensity',
+        label: 'Vapor Density:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'healthHazard',
+        label: 'Health Hazard:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'chemicalManufacturerID',
+        label: 'Chemical/Manf. ID:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'molWeight',
+        label: 'Molecular Weight (g/mol):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'flashPoint',
+        label: 'Flash Point (°F):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'meltingPoint',
+        label: 'Melting Point (°F):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'vaporPressure',
+        label: 'Vapor Pressure (mm/Hg):',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'ph',
+        label: 'pH:',
         value: ' ',
         required: false,
         order: 2
       }),
 
       new TextboxQuestion({
-        key: 'Description',
-        label: 'What is the Task being Assesed?',
+        key: 'lowerFlammability',
+        label: 'Lower Flammability (%):',
         value: ' ',
         required: false,
         order: 2
       }),
 
-      new CheckboxQuestion({
-        key: 'peopleEffected',
-        label: 'Who are the people affected by this task? ',
-        options: [
-          {key: 'operator',  value: 'Operator'},
-          {key: 'employees',  value: 'Employees'},
-          {key: 'visitors',   value: 'Visitors'},
-          {key: 'expectant-mothers',  value: 'New and expectant mothers'},
-          {key: 'disabled',  value: 'Disabled'},
-          {key: 'visitors',   value: 'Visitors'},
-          {key: 'contractors', value: 'Contractors'}
-        ],
-        order: 3
-      }),
-
+// Constituents Table
       new TextboxQuestion({
-        key: 'HazardousSubstances',
-        label: 'What hazardous substances are being used in this task?',
+        key: 'cas',
+        label: 'CAS#:',
         value: ' ',
         required: false,
         order: 2
-      })
+      }),
+      new TextboxQuestion({
+        key: 'constituentName',
+        label: 'Constituent Name:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'physicalState',
+        label: 'Physical State:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'percentOfChemical',
+        label: '% of chemical:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      // Exposure Limits - Within the Constituents Table
+      //ACGIH
+      new TextboxQuestion({
+        key: 'ACGIH_TLV',
+        label: 'ACGIH TLV:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'ACGIH_STEL',
+        label: 'ACGIH STEL:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'ACGIH_CL',
+        label: 'ACGIH CL:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'BEI',
+        label: 'BEI:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      // OSHA
+      new TextboxQuestion({
+        key: 'OSHA_PEL',
+        label: 'OSHA PEL:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'OSHA_STEL',
+        label: 'OSHA STEL:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'OSHA_CL',
+        label: 'OSHA CL:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'OSHA_EL',
+        label: 'OSHA EL:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      //NIOSH
+      new TextboxQuestion({
+        key: 'NIOSH_REL',
+        label: 'NIOSH REL:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'NIOSH_IDLH',
+        label: 'NIOSH IDLH:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'OdorThreshold',
+        label: 'Odor Threshold:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'OSHA_Standard',
+        label: 'OSHA Standard:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'exposureLimitsHealthHazard',
+        label: 'Health Hazard:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'OSHA_AirSamplingMethod',
+        label: 'OSHA Air Sampling Method:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'NIOSH_AirSamplingMethod',
+        label: 'NIOSH Air Sampling Method:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'bulkSamplingMethod',
+        label: 'Bulk Sampling Method:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+
+  // Work Conditions Table
+      new TextboxQuestion({
+        key: 'WorkSite',
+        label: 'Work Site:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'No_of_Workers',
+        label: 'No. of Workers:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'spaceVolume',
+        label: 'Space Volume:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'roomVentIn',
+        label: 'Room Ventilation In:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'roomVentOut',
+        label: 'Room Ventilation Out:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'No_of_Workers',
+        label: 'No. of Workers:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'temperature',
+        label: 'Temperature:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'humidity',
+        label: 'Humidity:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'windDirection',
+        label: 'Wind Direction:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'windSpeed',
+        label: 'Wind Speed:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'atmoPressure',
+        label: 'Atm. Pressure:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'workPosition',
+        label: 'Work Position:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'employeePosition',
+        label: 'Employee Position:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'DFBZ',
+        label: 'Distance from Breathing Zone:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'exposureFrequency',
+        label: 'Frequency of Exposure:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+      new TextboxQuestion({
+        key: 'exposurePerDay',
+        label: 'Duration of Exposure Per Day:',
+        value: ' ',
+        required: false,
+        order: 2
+      }),
+
+  // Task Table
+      //Task
+        // Material Fabrication
+          new TextboxQuestion({
+            key: 'generationRate',
+            label: 'Generation Rate (rods/hour) :',
+            value: ' ',
+            required: false,
+            order: 2
+          }),
+          new TextboxQuestion({
+            key: 'metalType',
+            label: 'Type of Metal Being Welded(Will Enter More at a Later Time) <Enter>:',
+            value: ' ',
+            required: false,
+            order: 2
+          }),
+          new TextboxQuestion({
+            key: 'exposurePerDay',
+            label: 'Duration of Exposure Per Day:',
+            value: ' ',
+            required: false,
+            order: 2
+          }),
+          new TextboxQuestion({
+            key: 'exposurePerDay',
+            label: 'Duration of Exposure Per Day:',
+            value: ' ',
+            required: false,
+            order: 2
+          }),
+          new TextboxQuestion({
+            key: 'exposurePerDay',
+            label: 'Duration of Exposure Per Day:',
+            value: ' ',
+            required: false,
+            order: 2
+          }),
+
+
+      // new CheckboxQuestion({
+      //   key: 'peopleEffected',
+      //   label: 'Who are the people affected by this task? ',
+      //   options: [
+      //     {key: 'operator',  value: 'Operator'},
+      //     {key: 'employees',  value: 'Employees'},
+      //     {key: 'visitors',   value: 'Visitors'},
+      //     {key: 'expectant-mothers',  value: 'New and expectant mothers'},
+      //     {key: 'disabled',  value: 'Disabled'},
+      //     {key: 'visitors',   value: 'Visitors'},
+      //     {key: 'contractors', value: 'Contractors'}
+      //   ],
+      //   order: 3
+      // }),
+      //
+      // new TextboxQuestion({
+      //   key: 'HazardousSubstances',
+      //   label: 'What hazardous substances are being used in this task?',
+      //   value: ' ',
+      //   required: false,
+      //   order: 2
+      // })
 
     ];
 
